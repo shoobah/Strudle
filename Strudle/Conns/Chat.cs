0d8c2308d2eac440b66f029c0f056c3670656fd9
shoobah <shoobah@gmail.com>
2013-03-18 22:49:43 +0100

    Added SocketExperiment project for testing SignalR client.

---
+++ Strudle/Conns/Chat.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace Strudle.Conns
+{
+    public class Chat : Hub
+    {
+        public void Send(string message)
+        {
+            // Call the addMessage method on all clients            
+            Clients.All.addMessage(string.Format("[{0}] says: {1}", Context.ConnectionId, message));
+        }
+    }
+}
