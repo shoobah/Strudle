9a803844d60ee9ef3ac3bc4957fcc8192ae1535c
xonium <johan.erik.obrink@gmail.com>
2013-03-21 21:44:51 +0100

    Litet test med refineSlide

---
+++ Strudle/Controllers/StrudleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Strudle.Controllers
+{
+    public class StrudleController : Controller
+    {
+        //
+        // GET: /Strudle/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
